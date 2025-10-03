3af43620b3ab746b9a72f9acbdeca4b29f6a9df4
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-03 10:42:47 -0300

    refactor(UserServiceTest): setting up our third test and also creating the validation for the first one

--- register/src/main/java/com/rafael/register/service/impl/UserServiceImpl.java
+++ register/src/main/java/com/rafael/register/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rafael.register.service.impl;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
+        validateUser(user);
         return repository.save(user);
     }
     
+    private void validateUser(User user) {
+        var age = Period.between(user.getBirthDate(), LocalDate.now()).getYears();
+
+        if (age < 18) {
+            throw new RuntimeException("User must be at least 18 years old to register.");
+        }
+
+        if (age > 60) {
+            throw new RuntimeException("User must be at most 60 years old to register.");
+        }
+    }
 }
