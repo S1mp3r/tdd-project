b3dbf784da67df5100a45fc9c49d8945938ff0e8
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-03 10:55:39 -0300

    refactor(UserService): refactoring our codebase to use Constaints instead of hardcoded value and also creating the validations for the other tests

--- register/src/main/java/com/rafael/register/service/impl/UserServiceImpl.java
+++ register/src/main/java/com/rafael/register/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.rafael.register.entity.User;
 import com.rafael.register.repository.UserRepository;
 import com.rafael.register.service.UserService;
+import com.rafael.register.utils.Constaints;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,12 +27,28 @@ public class UserServiceImpl implements UserService {
     private void validateUser(User user) {
         var age = Period.between(user.getBirthDate(), LocalDate.now()).getYears();
 
-        if (age < 18) {
+        if (age < Constaints.MIN_AGE) {
             throw new RuntimeException("User must be at least 18 years old to register.");
         }
 
-        if (age > 60) {
+        if (age > Constaints.MAX_AGE) {
             throw new RuntimeException("User must be at most 60 years old to register.");
+        }
+
+        if (user.getUsername().length() < Constaints.MIN_USERNAME_LENGTH) {
+            throw new RuntimeException("Username must be at least 3 characters long.");
+        }
+
+        if (user.getUsername().length() > Constaints.MAX_USERNAME_LENGTH) {
+            throw new RuntimeException("Username must be at most 10 characters long.");
+        }
+
+        if (user.getPassword().length() < Constaints.MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at least 4 characters long.");
+        }
+
+        if (user.getPassword().length() > Constaints.MAX_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at most 6 characters long.");
         }
     }
 }
