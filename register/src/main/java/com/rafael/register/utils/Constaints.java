5e7c8b879aeff08d3628f4201bd2c586289d1600
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-03 10:53:19 -0300

    feat(Utils): creating Constaints for a better codebase

---
+++ register/src/main/java/com/rafael/register/utils/Constaints.java
@@ -0,0 +1,14 @@
+package com.rafael.register.utils;
+
+public class Constaints {
+
+    private Constaints() {}
+
+    public static final int MIN_AGE = 18;
+    public static final int MAX_AGE = 60;
+    public static final int MIN_USERNAME_LENGTH = 3;
+    public static final int MAX_USERNAME_LENGTH = 10;
+    public static final int MIN_PASSWORD_LENGTH = 4;
+    public static final int MAX_PASSWORD_LENGTH = 6;
+
+}
